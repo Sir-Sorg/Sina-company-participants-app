18b048bca7a7a67c4ce8a59659d5926a72fe2c99
Sir-Sorg <sinaorojlo53@gmail.com>
2023-07-14 16:29:16 +0330

    Account stand

--- services/twitter_spider.py
+++ services/twitter_spider.py
@@ -5,11 +5,12 @@ USERNAME = 'josog43667'
 EMAIL = 'josog43667@msback.com'
 PASSWORD = 'ps4plus14'
 config.PROXY = {"http": "127.0.0.1:2080", "https": "127.0.0.1:2080"}
+ACCOUNT = Twitter_Conecction()
+
 
 def main():
-    user_account = Twitter_Conecction()
-    user_account.login(USERNAME, PASSWORD)
-    print(user_account.me)
+    ACCOUNT.login(USERNAME, PASSWORD)
+    print(ACCOUNT.me)
     # print(twitter.get_user_id('elonmusk'))
     # print(twitter.get_user_info('elonmusk'))
     # print(twitter.get_user_data('elonmusk'))
