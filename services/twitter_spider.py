912352e80b286542c2afcfbea712ef4ecb7738eb
Sir-Sorg <sinaorojlo53@gmail.com>
2023-07-14 16:15:55 +0330

    proxy set

--- services/twitter_spider.py
+++ services/twitter_spider.py
@@ -4,13 +4,12 @@ from core import config
 USERNAME = 'josog43667'
 EMAIL = 'josog43667@msback.com'
 PASSWORD = 'ps4plus14'
-
+config.PROXY = {"http": "127.0.0.1:2080", "https": "127.0.0.1:2080"}
 
 def main():
-    config.PROXY = {"http": "127.0.0.1:2080", "https": "127.0.0.1:2080"}
-    twitter = Twitter_Conecction()
-    twitter.login(USERNAME, PASSWORD)
-    print(twitter.me)
+    user_account = Twitter_Conecction()
+    user_account.login(USERNAME, PASSWORD)
+    print(user_account.me)
     # print(twitter.get_user_id('elonmusk'))
     # print(twitter.get_user_info('elonmusk'))
     # print(twitter.get_user_data('elonmusk'))
